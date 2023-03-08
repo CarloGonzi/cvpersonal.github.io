986d25d00c059aa1021e9eaa00c9f467191cf808
Carlos <125323164+CarloGonzi@users.noreply.github.com>
2023-03-08 19:32:00 -0300

    Initial commit

---
+++ pruebacv.js
@@ -0,0 +1,15 @@
+//Menu lateral
+var menu_visible = false;
+let menu = document.getElementById("nav");
+function mostrarOcultarMenu(){
+    if(menu_visible==false){//si está oculto
+        menu.style.display = "block";
+        menu_visible = true;
+    }
+    else{
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
+
+
